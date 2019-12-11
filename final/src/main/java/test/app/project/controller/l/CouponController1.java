5d274405f8b05e5539e1fa46e5f34ae275df4f0c
leejunbin <junbin900702@gmail.com>
2019-12-11 10:04:23 +0900

    2019-12-11 오전 10시 5분_쿠폰수정_이준빈

--- final/src/main/java/test/app/project/controller/l/CouponController1.java
+++ final/src/main/java/test/app/project/controller/l/CouponController1.java
@@ -21,13 +21,13 @@ public class CouponController1 {
 	public String couponInfo(String gnum,String coupon_type,String coupon_name){
 		HashMap<String, Object> map=new HashMap<String, Object>();
 		System.out.println("gnum: " + gnum);
-		map.put("ggnum", gnum);
+		map.put("gnum", gnum);
 		//등급에 해당하는 인원수 구함
 		int membersCnt=service.countMembers(map);
 		System.out.println("membersCnt: " + membersCnt);
 		List<MembersVo> mlist=service.membersId(map);
 		for(MembersVo vo:mlist){
-			//System.out.println(vo.getMid());
+			System.out.println(vo.getMid());
 			CouponVo cvo=new CouponVo(0, vo.getMid(), coupon_name, coupon_type, null, null);
 			service.couponInsert(cvo);
 		}
@@ -37,10 +37,10 @@ public class CouponController1 {
 		sb.append("<result>");
 		sb.append("<user>");
 		for(MembersVo vo:mlist){
-			sb.append(vo.getMid()+"  ");
+			sb.append("'"+vo.getMid()+"'  ");
 		}
 		sb.append("</user>");
-		sb.append("<msg>님들에게 쿠폰 발송 완료!!</msg>");
+		sb.append("<msg>님에게 쿠폰 발송 완료!!</msg>");
 		sb.append("</result>");
 		return sb.toString();
 	}
